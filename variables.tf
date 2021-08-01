607b2f173a7384c890c7d2f562c2d3548866126b
Leandro Lopez <leandro.a.lopez@wearewaes.com>
2021-08-01 13:33:33 +0200

    add msi

--- variables.tf
+++ variables.tf
@@ -1,5 +1,24 @@
 variable "ssadminpassword" {
   type = string
-  default = "Ingeniero1991@"
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+  sensitive = true
+}
+
+variable "client_id" {
+  type = string
+  sensitive = true
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "subscription_id" {
+  type = string
   sensitive = true
 }
